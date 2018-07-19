880b631e8fd27cdd5fff37aae92e0990641b1078
AKhatskevich <avkhatskevich@tarantool.org>
2018-07-19 19:02:17 +0300

    Add docs on default values

--- test/ddt_suite/record_default.lua
+++ test/ddt_suite/record_default.lua
@@ -117,3 +117,78 @@ t {
     input = [[ [[1, 2, null], "str" ] ]],
     output = [[ {"foo": {"f1": 1, "f2": 2, "f3": null}, "bar": "str"} ]],
 }
+
+local default_inside_of_default = [[
+{
+    "type": "record",
+    "name": "Frob",
+    "fields": [
+        { "name": "foo", "type":
+            { "type": "record*", "name": "default_record", "fields":[
+                {"name": "f1", "type": "int"},
+                {"name": "f2", "type": "int"},
+                {"name": "f3", "type":
+                    {"type": "record*", "name": "default_record_2", "fields":[
+                        {"name": "f3_1", "type": "int*"}
+                    ]},
+                "default": {"f3_1": 3}}
+            ]}, "default": { "f1": 1, "f2": 2}},
+        { "name": "bar", "type": "string" }
+    ]
+}
+]]
+
+t {
+    schema = default_inside_of_default,
+    func = "flatten",
+    input = [[ {"bar": "str"} ]],
+    output = [=[ [[1, 2, [3]], "str" ] ]=],
+}
+
+t {
+    schema = default_inside_of_default,
+    func = "flatten",
+    input = [[ {"foo": {"f1":5, "f2":7}, "bar": "str"} ]],
+    output = [=[ [[5, 7, [3]], "str" ] ]=],
+}
+
+t {
+    schema = default_inside_of_default,
+    func = "flatten",
+    input = [[ {"foo": {"f1":5, "f2":7, "f3": null}, "bar": "str"} ]],
+    output = [=[ [[5, 7, null], "str" ] ]=],
+}
+
+local default_inside_of_default_2 = [[
+{
+    "type": "record",
+    "name": "Frob",
+    "fields": [
+        { "name": "foo", "type":
+            { "type": "record*", "name": "default_record", "fields":[
+                {"name": "f1", "type": "int"},
+                {"name": "f2", "type": "int"},
+                {"name": "f3", "type":
+                    {"type": "record*", "name": "default_record_2", "fields":[
+                        {"name": "f3_1", "type": "int*", "default": 3}
+                    ]}
+                }
+            ]}, "default": { "f1": 1, "f2": 2, "f3": null}},
+        { "name": "bar", "type": "string" }
+    ]
+}
+]]
+
+t {
+    schema = default_inside_of_default_2,
+    func = "flatten",
+    input = [[ {"bar": "str"} ]],
+    output = [=[ [[1, 2, null], "str" ] ]=],
+}
+
+t {
+    schema = default_inside_of_default_2,
+    func = "flatten",
+    input = [[ {"foo": {"f1":5, "f2":7, "f3": {}}, "bar": "str"} ]],
+    output = [=[ [[5, 7, [3]], "str" ] ]=],
+}
