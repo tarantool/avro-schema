dd36084c06bbe2e7657a6eabf75790d493cb91c7
Alexander Turenko <alexander.turenko@tarantool.org>
2019-10-23 18:57:10 +0300

    test: fix module reload test
    This commit follows up bbcf5fc14e3fd1962351574d3f6c30e84bcf7c92 ('fix:
    allow to reload module') and fixes the test by passing correct LUA_PATH.
    
    Enhanced the test itself a bit: added first require (w/o it the case
    will pass, but will not actually verify reloading), add an error to the
    `ctest -VV` output if the test will fail, added comments.
    
    Follows up #34.

--- test/api_tests/reload.lua
+++ test/api_tests/reload.lua
@@ -5,10 +5,17 @@ test:plan(1)
 
 test:test('reload test', function(test)
     test:plan(1)
+
+    -- Require the module first time.
+    require('avro_schema')
+
+    -- Unload it.
     package.loaded['avro_schema'] = nil
     package.loaded['avro_schema.il'] = nil
-    local ok = pcall(require, 'avro_schema')
-    test:ok(ok, 'Successfully reloaded')
+
+    -- Require it again.
+    local ok, err = pcall(require, 'avro_schema')
+    test:ok(ok, 'Successfully reloaded', {err = err})
 end)
 
 os.exit(test:check() and 0 or 1)
